ce7f86615c3c78a5771e6975d09ef40441332bc6
dododove <twoleapahead@gmail.com>
2023-07-31 12:49:42 +0900

    Second modification

---
+++ overdrive.cpp
@@ -0,0 +1,11 @@
+#include "dspinst.h"
+//Function Definition
+void AudioEffectCubicDistort::update(void)
+{
+    audio_block_t *audioblock;
+    int16_t *data, *end;
+    int32_t calc, temp;
+    audioblock = 
+}
+
+//Variable Declare
